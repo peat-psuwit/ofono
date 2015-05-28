94eacc90dcda7dd10180db1fe3f95ab34ccf2771
Ratchanan Srirattanamet <peathot@hotmail.com>
2015-05-28 23:55:51 +0700

    qcommsimmodem: Add new radio-settings driver for qcommsimmodem.
    This will handle setting radio mode to ohter mode then GSM when
    other SIM's mode is not GSM. This is limitation of this modem,
    where only 1 SIM slot can prefer UMTS at a time.

---
+++ drivers/qcommsimmodem/qcom_msim_modem.h
@@ -0,0 +1,25 @@
+/*
+ *
+ *  oFono - Open Source Telephony - RIL Modem Support
+ *
+ *  Copyright (C) 2015 Ratchanan Srirattanamet
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#define QCOMMSIMMODEM "qcommsimmodem"
+
+extern void qcom_msim_radio_settings_init(void);
+extern void qcom_msim_radio_settings_exit(void);
